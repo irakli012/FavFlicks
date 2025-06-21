26208c024c7ccc29e223b9d7a09edb4a0ec44863
irakli012 <iraklimate@gmail.com>
2025-06-22 01:34:44 +0400

    add must have models

---
+++ data/Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.data.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public ICollection<MovieTag> MovieTags { get; set; } = new List <MovieTag>();
+    }
+}
