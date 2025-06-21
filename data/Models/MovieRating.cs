33091401c1ac794a7c7a1ad1285a7931c2a58a1c
Iraklimate <iraklimate@gmail.com>
2025-06-22 01:34:44 +0400

    add must have models

---
+++ data/Models/MovieRating.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using favflicks.data.Enums;
+
+namespace favflicks.data.Models
+{
+    public class MovieRating
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public AppUser User { get; set; } = null!;
+        public Rating Value { get; set; } // 1 to 10
+    }
+
+}
---
+++ data/Models/MovieTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.data.Models
+{
+    public class MovieTag
+    {
+        public int MovieId {  get; set; }
+        public Movie Movie { get; set; } = null!;
+
+        public int TagId { get; set; }
+        public Tag Tag { get; set; } = null!;
+    }
+}
