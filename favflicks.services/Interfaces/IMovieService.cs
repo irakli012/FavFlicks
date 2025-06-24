408332b861dbe6642b39b41d7fd039d0562368aa
irakli012 <iraklimate@gmail.com>
2025-06-24 21:38:03 +0400

    create service and interface for movies and add simple crud

---
+++ favflicks.services/Interfaces/IMovieService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using favflicks.data.Models;
+
+namespace favflicks.services.Interfaces
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<Movie>> GetMovieListAsync();
+        Task<Movie?> GetMovieByIdAsync(int id);
+        Task AddAsync(Movie movie);
+        Task UpdateAsync(Movie movie);
+        Task DeleteAsync(int id);
+    }
+}
