6ed5c1fd41aff8e0ebfc9e0b21de8f159e55f371
Iraklimate <iraklimate@gmail.com>
2025-06-27 03:00:45 +0400

    add interface and service for comment

---
+++ favflicks.services/Interfaces/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using favflicks.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.services.Interfaces
+{
+    public interface CommentService
+    {
+        Task<IEnumerable<Comment>> GetCommentsByMovieIdAsync(int movieId);
+        Task<Comment> GetCommentsByIdAsync(int id);
+        Task AddAsync(Comment comment);
+        Task UpdateAsync(Comment comment);
+        Task DeleteAsync(int id);
+    }
+}
