7f38a948ca4221cd579ff0b6695cbee6fb633cf5
irakli012 <iraklimate@gmail.com>
2025-07-01 03:33:31 +0400

    add tag interface and service\

---
+++ favflicks.services/Interfaces/ITagService.cs
@@ -0,0 +1,18 @@
+﻿using favflicks.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.services.Interfaces
+{
+    public interface ITagService
+    {
+        Task<IEnumerable<Tag>> GetAllAsync();
+        Task<Tag?> GetByIdAsync(int id);
+        Task AddAsync(Tag tag);
+        Task UpdateAsync(Tag tag);
+        Task DeleteAsync(int id);
+    }
+}
