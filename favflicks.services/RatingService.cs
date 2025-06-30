35bb205991d8e367c7c9bec5d75b99d09b5ea600
irakli012 <iraklimate@gmail.com>
2025-07-01 01:58:59 +0400

    added rating service and interface (last commits didnt upload logic)

--- favflicks.services/RatingService.cs
+++ favflicks.services/RatingService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using favflicks.data;
+using favflicks.data.Enums;
+using favflicks.data.Models;
+using favflicks.services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,46 @@ using System.Threading.Tasks;
 
 namespace favflicks.services
 {
-    internal class RatingService
+    public class RatingService(AppDbContext context) : IRatingService
     {
+        public async Task<IEnumerable<MovieRating>> GetRatingsByMovieIdAsync(int movieId)
+        {
+            return await context.MovieRatings
+                .Where( r => r.MovieId == movieId)
+                .Include(r => r.Movie)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<MovieRating>> GetRatingsByUserIdAsync(String userId)
+        {
+            return await context.MovieRatings
+                .Where ( r => r.UserId == userId)
+                .Include(r => r.Movie)
+                .ToListAsync();
+        }
+
+        public async Task<MovieRating?> GetByIdAsync(int id)
+        {
+            return await context.MovieRatings
+                .Include(r => r.Movie)
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task AddOrUpdateAsync(MovieRating rating)
+        {
+            var existing = await context.MovieRatings
+                .FirstOrDefaultAsync(r => r.MovieId == rating.MovieId && r.UserId == rating.UserId);
+
+            if (existing != null)
+            {
+                existing.Value = rating.Value;
+            }
+            else
+            {
+                context.MovieRatings.Add(rating);
+            }
+
+            await context.SaveChangesAsync();
+        }
     }
 }
