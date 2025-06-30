4d87e759bf3baff1273cfcea308a977e19782795
Iraklimate <iraklimate@gmail.com>
2025-07-01 03:34:36 +0400

    add tag controller

---
+++ favflicks/Controllers/TagController.cs
@@ -0,0 +1,50 @@
+﻿using favflicks.data.Models;
+using favflicks.services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace favflicks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController(ITagService tagService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tag>>> GetAll()
+        {
+            var tags = await tagService.GetAllAsync();
+            return Ok(tags);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tag>> GetById(int id)
+        {
+            var tag = await tagService.GetByIdAsync(id);
+            if (tag == null)
+                return NotFound();
+            return Ok(tag);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] Tag tag)
+        {
+            await tagService.AddAsync(tag);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] Tag tag)
+        {
+            if (id != tag.Id) return BadRequest("ID mismatch");
+            await tagService.UpdateAsync(tag);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await tagService.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
