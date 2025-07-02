7f15d61e880e362b83f70809d1059ffa205a141f
irakli012 <iraklimate@gmail.com>
2025-07-03 00:13:17 +0400

    add interface and service for auth

---
+++ favflicks.services/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using favflicks.data.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string?> RegisterAsync(RegistorDto dto);
+        Task<string?> LoginAsync(LoginDto dto);
+    }
+}
