6edb92a3c809cc2648bb7054fb30898a9a6b4508
Iraklimate <iraklimate@gmail.com>
2025-07-03 00:37:32 +0400

    add auth controller

---
+++ favflicks/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using favflicks.data.Dtos;
+using favflicks.services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace favflicks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService authService) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var token = await authService.RegisterAsync(dto);
+            if (token != null)
+                return BadRequest("registration failed");
+            return Ok(new { token });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var token = await authService.LoginAsync(dto);
+            if (token == null) return Unauthorized();
+            return Ok(new { token });
+        }
+
+    }
+}
