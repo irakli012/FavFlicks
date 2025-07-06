fcb07c2b403a9546ddf9e93ae0d2d8f6c5f06d11
irakli012 <iraklimate@gmail.com>
2025-07-06 05:44:25 +0400

    make userid nulable and delete required

---
+++ favflicks.data/Migrations/20250706013325_MakeUserIdNotRequired.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace favflicks.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeUserIdNotRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
