0f17f30c81d486c937149f9ccf8d4a63bb4d2615
Iraklimate <iraklimate@gmail.com>
2025-07-17 23:29:28 +0400

    add new model watchlist

---
+++ favflicks.data/Models/WatchList.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace favflicks.data.Models
+{
+    public class WatchList
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public AppUser? User { get; set; }
+
+        [Required]
+        public int MovieId { get; set; }
+        public Movie? Movie { get; set; }
+
+        public DateTime AddedDate { get; set; } = DateTime.UtcNow;
+    }
+}
