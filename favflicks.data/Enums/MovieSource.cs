1a3185933b6da4f8a2d36a0a51ae2cb7400fa8e8
irakli012 <iraklimate@gmail.com>
2025-07-29 04:26:57 +0400

    add movie source enum

---
+++ favflicks.data/Enums/MovieSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.data.Enums
+{
+    public enum MovieSource
+    {
+        TMDB = 1,
+        UserImport = 2
+    }
+}
