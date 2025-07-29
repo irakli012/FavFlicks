233660988b09b13021aa217ec0d240883606e56c
irakli012 <iraklimate@gmail.com>
2025-07-29 04:26:35 +0400

    add models for tmdb (external movies db api)

---
+++ favflicks.data/Models/TMDB/TmdbCredits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.data.Models.TMDB
+{
+    public class TmdbCredits
+    {
+        public List<TmdbCast> Cast { get; set; }
+        public List<TmdbCrew> Crew { get; set; }
+    }
+}
---
+++ favflicks.data/Models/TMDB/TmdbMovieResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.data.Models.TMDB
+{
+    public class TmdbMovieResult
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Overview { get; set; }
+        public string PosterPath { get; set; }
+        public string BackdropPath { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public double VoteAverage { get; set; }
+    }
+}
