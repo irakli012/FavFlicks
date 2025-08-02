001a9af9b8d7094538d3222d93fcd0be0df0a49d
irakli012 <iraklimate@gmail.com>
2025-08-02 20:54:00 +0400

    add more tmdb models

---
+++ favflicks.data/Models/TMDB/TmdbVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.data.Models.TMDB
+{
+    public class TmdbVideo
+    {
+        public string Key { get; set; } = null!;
+        public string Site { get; set; } = null!;
+        public string Type { get; set; } = null!;
+    }
+}
---
+++ favflicks.data/Models/TMDB/TmdbVideoResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favflicks.data.Models.TMDB
+{
+    public class TmdbVideosResponse
+    {
+        public int Id { get; set; }
+        public List<TmdbVideo> Results { get; set; } = new();
+    }
+}
