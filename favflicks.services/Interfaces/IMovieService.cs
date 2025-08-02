28fae7164f03abbec7a818c783277c3839db336c
irakli012 <iraklimate@gmail.com>
2025-08-02 20:56:44 +0400

    modify movie service for new functionality

--- favflicks.services/Interfaces/IMovieService.cs
+++ favflicks.services/Interfaces/IMovieService.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using favflicks.data.Enums;
 using favflicks.data.Models;
 
 namespace favflicks.services.Interfaces
 {
     public interface IMovieService
     {
+        // Local database operations
         Task<IEnumerable<Movie>> GetMovieListAsync(string userId);
         Task<Movie?> GetMovieByIdAsync(int id, string userId);
-        Task AddAsync(Movie movie);
+        Task<Movie> AddManualMovieAsync(Movie movie, string userId);
         Task UpdateAsync(Movie movie);
         Task DeleteAsync(int id);
+
+        // TMDB API operations
+        Task<Movie> ImportFromTmdbAsync(int tmdbId, string userId);
+        Task<IEnumerable<Movie>> SearchTmdbMoviesAsync(string query);
+
+        // Fetch API operations
+        Task<IEnumerable<Movie>> SearchAllSourcesAsync(string query);
+        Task<Movie?> GetMovieFromAllSourcesAsync(int id, string source, string userId);
     }
 }
