9934f1aa4ae800da9ec5b2c2eee3c874875c6517
irakli012 <iraklimate@gmail.com>
2026-01-28 16:33:14 +0400

    Fix CORS to allow Vercel and localhost origins

--- favflicks/Program.cs
+++ favflicks/Program.cs
@@ -90,7 +90,11 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173")
+                          policy.WithOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:3000",
+                                "https://*.vercel.app"
+                            )
                                 .AllowAnyHeader()
                                 .AllowAnyMethod()
                                 .AllowCredentials();
