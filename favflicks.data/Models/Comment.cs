98bc870fcd171e6e977b3a0f6f7f6952f072e27b
i.matiashvili <i.matiashvili@payunicard.ge>
2026-02-20 18:39:57 +0400

    modify readme and use scalar instead of swagger

--- favflicks.data/Models/Comment.cs
+++ favflicks.data/Models/Comment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace favflicks.data.Models
@@ -12,9 +13,11 @@ namespace favflicks.data.Models
         public string Content { get; set; } = null!;
 
         public int MovieId { get; set; }
+        [JsonIgnore]
         public Movie? Movie { get; set; }
 
         public string UserId { get; set; } = null!; 
+        [JsonIgnore]
         public AppUser? User { get; set; }
     }
 
--- favflicks.data/Models/Favorite.cs
+++ favflicks.data/Models/Favorite.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace favflicks.data.Models
@@ -11,9 +12,11 @@ namespace favflicks.data.Models
         public int Id { get; set; }
 
         public int MovieId { get; set; }
+        [JsonIgnore]
         public Movie? Movie { get; set; }
 
         public string UserId { get; set; }
+        [JsonIgnore]
         public AppUser? User { get; set; }
     }
 }
